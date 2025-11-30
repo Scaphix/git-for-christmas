b0991d03f149d24cefad258104e903335fd3b3c0
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-11-30 13:03:18 +0100

    Update SECRET_KEY handling in settings to use environment variable

--- gitforchristmas/settings.py
+++ gitforchristmas/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#dw6p-_(fs%5$84ps8xeulipjtgb141aij3pk1&a@4@de+8m$d'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#dw6p-_(fs%5$84ps8xeulipjtgb141aij3pk1&a@4@de+8m$d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
