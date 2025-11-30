01e7bb64d13220cacc06743cd344144413861bc4
Julia <juliadvolponi@gmail.com>
2025-11-30 17:15:18 +0000

    created a file urls.py inside matches to link all the files

---
+++ matches/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # API to register a new player 
+    path('register/', views.register_player, name='register_player'),
+    
+    # API to get the current list of players 
+    path('players/', views.get_players, name='get_players'),
+    
+    # 3. API to run the matching 
+    path('run/', views.run_matching_logic, name='run_matching'),
+    
+    # 4. API to look up a player's assigned match
+    path('lookup/', views.get_user_match, name='get_user_match'),
+]
